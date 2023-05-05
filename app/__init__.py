884aa6e1369fd8c31b3e1f059978eac491bc2e01
Hitha Shri <hithashrinagaruru@gmail.com>
2023-05-05 12:53:47 +0530

    database credentials added 2

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="dpg-chaagk2k728r886hkq80-a.oregon-postgres.render.com/todo_2xh3",
+        host="dpg-chaagk2k728r886hkq80-a.oregon-postgres.render.com",
         database="todo_2xh3",
         user="todo_2xh3_user",
         password="T29sKtf4VrI1C0y6K3AfUzM81Zn30cwh")
